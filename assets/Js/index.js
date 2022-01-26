ef8c678e54bb7776335340d6505fa97f2ef5a29f
kevinNdahayo <ndahayokevin1@gmail.com>
2022-01-25 23:32:23 -0800

    New async javascript files

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,3 +1,4 @@
+
 // Old javascript 
 var counter = 5;
 
@@ -167,3 +168,177 @@ const hasInexpensiveItems = items.some( item => {
 })
 
 console.log("Inexpensive Items ", hasInexpensiveItems)
+
+
+// Array method: Reduce method
+const numbers = [1,-1,2,3]
+
+const sum = numbers.reduce((accumulator , currentvalue) => {
+	return accumulator + currentvalue; 
+}, 0);
+
+console.log(sum)
+
+// challenges
+const listOfNumbers = [1,2,3,5,7,11];
+const sumOfNums = listOfNumbers.reduce ((cummulatorValue , currentValue) => {
+	return cummulatorValue + currentValue;
+},0);
+
+console.log(sumOfNums);
+
+
+// Javascript callback functions
+
+const x = function(){
+	console.log('This function is called inside a function');
+}
+
+const y = function (callback){
+	console.log('Do something')
+	callback();
+}
+
+y(x);
+
+// function 2
+
+const calc = function( num1,num2, calcType){
+	if(calcType == "add") {
+		return num1 + num2;
+	}else if(calcType == "substract") {
+	    if(num1 > num2){
+		return num1 - num2;
+	    }else{
+		return num2 - num1;
+	    }
+	}else if(calcType == "multiply") {
+		return num1 * num2;
+	}else if(calcType == "divide") {
+		if(num1 > num2 ){
+			return num1 / num2;
+		}else{
+			return num2 / num1;
+		}
+	}else{
+		return "The Calculation symbol should be either '+', '-', '*' or '/' ";
+	}
+}
+
+const solutionAddition = calc(2,4,"add");
+const solutionSubstract = calc(1,3,"substract");
+const solutionMultiply = calc(3, 5, "multiply");
+const solutionDivide = calc(5, 5 , "divide");
+
+console.log(solutionAddition);
+console.log(solutionSubstract);
+console.log(solutionMultiply);
+console.log(solutionDivide);
+
+// Using the callback functions to do some computations
+
+let add = function(a , b){
+	return a * b; 
+}
+
+const calculate = function (num1 , num2 , callback){
+	return callback(num1 , num2);
+}
+
+
+// New part: Javascript Promises
+/*
+// if promise is fullfilled
+const fullFillment = () => {
+	// log successfullment message
+	console.log('The table is set up for Chips !!!')
+}
+
+const onRejection = () => {
+	console.log('The table is set up for Snacks !!!')
+}
+const promise = new Promise((resolve, rejection) => {
+	setTimeout(() => {
+		// Food truck is found 
+		// change the status from 'pending' to 'fullfilled'
+		resolve()
+	}, 5000) 
+})
+
+// Reject scenario 
+const promise = new Promise((resolve , reject) => {
+	setTimeout(() => {
+		// Food truck is not found
+		// change the status from 'Pending' to 'Rejected'
+		reject()
+	}, 5000)
+
+})
+
+promise.then(fullFillment);
+promise.catch(onRejection);
+
+*/
+
+// Async javascript from traversy media
+
+/*
+const posts = [
+	{ title: 'Post one', body: 'This is post one' },
+	{ title: 'Post two', body: 'This is post two' },
+	{ title: 'Post three', body: 'This is post three' },
+	{ title: 'Post four', body: 'This is post four' }
+
+]
+
+const getPosts = () => {
+	setTimeout(() => {
+		let output = '';
+		posts.forEach((post, index) => {
+			output += `<li>${post.title}</li>`
+		});
+		document.body.innerHtml = output;
+	}, 1000)
+}
+
+getPosts();
+
+*/
+
+// Async javascript from CodeSackr
+// Callback function
+
+let greeting = name => console.log(`Hello ${name}`)
+
+const userInfo = (firstName , lastName , callback) =>  {
+	const fullName = `${firstName} ${lastName}`;
+	callback(fullName);
+}
+
+userInfo('John','Doe', greeting)
+
+// Promises in javascript
+
+const hasMeeting = false;
+
+const meeting = new Promise((resolve, reject) => {
+	if(!hasMeeting) {
+		const meetingDetails = {
+			name: "Marketing details",
+			location: "Skype",
+			time: "1:00 PM"
+		}
+	resolve(meetingDetails);
+	} else {
+		reject( new Error('Meeting already scheduled !!!'))
+	}	
+})
+
+meeting
+	.then( res => {
+		console.log('Meeting scheduled')
+		console.log(res)
+	})
+	.catch(err => {
+		console.log(err.message)
+	})
